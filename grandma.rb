a5214846258eea70cf0ec09853869831cbb2cd9b
Britny Edwards <bredwards2@gmail.com>
2018-11-10 00:26:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
-  return HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
   
   elsif phrase == phrase.upcase && phrase != "I LOVE YOU, GRANDMA!""
   return "No, not since 1938!""
